7e2454211b4a66647afd69ca3971873cf0d59d59
Fernando Tschopp <tschoppfernando@gmail.com>
2021-03-25 22:38:13 -0300

    fix: datepicker styles

--- src/lib/ui-core/datepicker/parts/CalendarInput.component.jsx
+++ src/lib/ui-core/datepicker/parts/CalendarInput.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function CalendarInput({ name, value, onClick, hasError }) {
-  const iconClass = `flex z-10 leading-snug font-normal absolute text-center absolute bg-transparent rounded text-base items-center justify-center h-8 w-8 right-0 cursor-pointer`;
+  const iconClass = `flex z-10 leading-snug font-normal absolute text-center absolute bg-transparent rounded text-base items-center justify-center h-7 w-7 right-0 cursor-pointer`;
   const errorClasses = `text-red-300 bg-red-100 border-red-300 
     focus:border-red-300 placeholder-red-600`;
   return (
-    <div className="flex w-full max-h-8 h-8">
+    <div className="flex w-full max-h-7 h-7">
       <input
         name={name}
         readOnly={true}
@@ -17,21 +17,19 @@ function CalendarInput({ name, value, onClick, hasError }) {
         onClick={onClick}
       />
       <span className={iconClass} onClick={onClick}>
-        <div className="px-3 py-2">
-          <svg
-            className="h-6 w-6 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-            />
-          </svg>
-        </div>
+        <svg
+          className="h-5 w-5 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+          />
+        </svg>        
       </span>
     </div>
   );
