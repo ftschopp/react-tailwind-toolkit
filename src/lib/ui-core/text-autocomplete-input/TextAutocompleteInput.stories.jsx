52daacdb79a1b0487955cef69c6e0471f0484c23
Fernando Tschopp <tschoppfernando@gmail.com>
2021-06-26 13:26:38 -0300

    fix: problems with textautocomplete (#36)

--- src/lib/ui-core/text-autocomplete-input/TextAutocompleteInput.stories.jsx
+++ src/lib/ui-core/text-autocomplete-input/TextAutocompleteInput.stories.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import TextAutocompleteInput from './TextAutocompleteInput.component';
 import { withKnobs } from '@storybook/addon-knobs';
+import { filter, compose, includes, propOr, toLower } from 'ramda';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -20,29 +21,54 @@ export default {
 export const Standard = args => {
   // const [value, setValue] = useState('Juan');
 
-  const options = [
-    { name: 'Juan Pedro Hacha', document: 'Legajo 78411515' },
-    { name: 'Juan Carlos Gonzalez', document: 'Legajo 78411515' },
-    { name: 'Juan Pablo Gonzalez', document: 'Legajo 78411515' },
-    { name: 'Juana Lopez', document: 'Legajo 78411515' },
-    { name: 'Fernando Tschoppp', document: 'Legajo 33035151' },
-    { name: 'Pablo Garcia', document: 'Legajo 342566' },
-    { name: 'Samantha Se Labanca', document: 'Legajo 1111111' },
-    { name: 'Maria Lopez', document: 'Legajo 67895432677' },
+  const employees = [
+    { id: '1', name: 'Juan Pedro Hacha', document: 'Legajo 78411515' },
+    { id: '2', name: 'Juan Carlos Gonzalez', document: 'Legajo 78411515' },
+    { id: '3', name: 'Juan Pablo Gonzalez', document: 'Legajo 78411515' },
+    { id: '4', name: 'Juana Lopez', document: 'Legajo 78411515' },
+    { id: '5', name: 'Fernando Tschoppp', document: 'Legajo 33035151' },
+    { id: '6', name: 'Pablo Garcia', document: 'Legajo 342566' },
+    { id: '7', name: 'Samantha Se Labanca', document: 'Legajo 1111111' },
+    { id: '8', name: 'Maria Lopez', document: 'Legajo 67895432677' },
   ];
+
+  const [open, setOpen] = useState(false);
+  const getEmployeesFiltered = useCallback(
+    (lista, value) =>
+      filter(compose(includes(toLower(value)), toLower, propOr('', 'name')))(employees),
+    [employees],
+  );
+  const [listaEmployees, setListaEmployees] = useState([]);
+  const [value, setValue] = useState();
+  const onSearch = useCallback(
+    value => {
+      setTimeout(() => {
+        const lista = value !== '' ? getEmployeesFiltered(employees, value) : [];
+        setListaEmployees(lista);
+      }, 800);
+    },
+    [employees, getEmployeesFiltered],
+  );
+
+  const handleChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <div>
       <TextAutocompleteInput
-        label="Username"
-        {...args}
-        className="w-64"
-        options={options}
+        name="employeeName"
+        type="text"
         fieldValue="name"
         fieldTitle="document"
-        onSearch={e => alert('on search: ' + e)}
-        error=""
-        touched={false}
-        onOptionClick={e => alert(JSON.stringify(e))}
+        placeholder="Ingrese una descripción general"
+        value={value}
+        options={listaEmployees}
+        onSearch={onSearch}
+        onChange={handleChange}
+        onOptionClick={opt => {
+          setValue(opt.name);
+        }}
       />
       <h1>algo</h1>
     </div>
